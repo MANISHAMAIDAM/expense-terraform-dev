44b89824b0c4d2abb9d34ba8ec7194303df11089
manishamaidam <maidammanisha.gmail.com>
2025-05-04 19:22:07 +0530

    exp-dev

--- 01-vpc/vpc.tf
+++ 01-vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/daws-78s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/MANISHAMAIDAM/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
@@ -8,3 +8,6 @@ module "vpc" {
     database_subnet_cidrs = var.database_subnet_cidrs
     is_peering_required = var.is_peering_required
 }
+
+
+
